00ac5aba1a9172ab5a999d9b20358d1e2ee3712d
Mohamed Samir <msalem@egypt.itgsolutions.com>
2020-10-28 15:35:23 +0200

    App structure has bee created

---
+++ lib/resources/images.dart
@@ -0,0 +1,8 @@
+const String IMAGE_ROOT = "assets/";
+
+const String INTERFACE1 = IMAGE_ROOT + "interFace1.png";
+const String INTERFACE2 = IMAGE_ROOT + "interFace2.png";
+const String INTERFACE3 = IMAGE_ROOT + "interFace3.png";
+const String INTERFACE4 = IMAGE_ROOT + "interFace4.png";
+const String USER_IMAGE = IMAGE_ROOT + "userImage.png";
+const String WEB_INTERFACE = IMAGE_ROOT + "webInterFace.png";
