f93e1f96ba9fa72e36b16d321d23d209b80ab4a0
Mohamed Samir <msalem@egypt.itgsolutions.com>
2020-10-28 16:25:40 +0200

    build first image in course details

--- lib/CourseDesignDetails/view/CourseCardDetails.dart
+++ lib/CourseDesignDetails/view/CourseCardDetails.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ui_task/resources/images.dart';
 
 class CourseCardDetails extends StatefulWidget {
-  CourseCardDetails({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _CourseCardDetailsState createState() => _CourseCardDetailsState();
 }
 
 class _CourseCardDetailsState extends State<CourseCardDetails> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Image(
+                height: MediaQuery.of(context).size.height / 1.7,
+                fit: BoxFit.fill,
+                image: AssetImage(INTERFACE4),
+              )),
+        ],
+      ),
+    );
+  }
 }
