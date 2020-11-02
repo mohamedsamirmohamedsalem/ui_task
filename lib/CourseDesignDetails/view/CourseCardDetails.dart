002fab1a926eba87411c903fd2dfc6d19a335cc6
msalem <msalem@egypt.itgsolutions.com>
2020-11-02 13:02:02 +0200

    sepreate the screen into 2 section and build design till texts os second section

--- lib/CourseDesignDetails/view/CourseCardDetails.dart
+++ lib/CourseDesignDetails/view/CourseCardDetails.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ui_task/resources/Dimens.dart';
+import 'package:ui_task/resources/colors.dart';
+import 'package:ui_task/resources/constants.dart';
 import 'package:ui_task/resources/images.dart';
 
 class CourseCardDetails extends StatefulWidget {
@@ -11,21 +14,70 @@ class _CourseCardDetailsState extends State<CourseCardDetails> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
+      color: Colors.yellow,
+      child: Stack(
+        alignment: Alignment.topLeft,
         children: [
-          Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Image(
-                height: MediaQuery.of(context).size.height / 1.7,
-                fit: BoxFit.fill,
-                image: AssetImage(INTERFACE4),
-              )),
+          buildTopImage(),
+          buildBottomSection(),
         ],
       ),
+    );
+  }
+
+  // MARK:- UI Layer
+
+  Image buildTopImage() {
+    return Image(
+      height: MediaQuery.of(context).size.height / 2.3 + 40,
+      fit: BoxFit.fill,
+      image: AssetImage(INTERFACE4),
+    );
+  }
+
+  Positioned buildBottomSection() {
+    return Positioned.fill(
+      top: MediaQuery.of(context).size.height / 2.2,
+      child: Stack(
+        alignment: Alignment.topLeft,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: DIMEN_26),
+            decoration: boxDecorationSide20,
+            child: buildListOfTitles(),
+          ),
+          buildFavoriteCircleAvatar(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildFavoriteCircleAvatar() {
+    return Align(
+      alignment: Alignment.topRight,
+      child: Padding(
+          padding: EdgeInsets.only(right: DIMEN_25, top: DIMEN_1),
+          child: CircleAvatar(
+            backgroundColor: BLUE,
+            radius: DIMEN_26,
+            child: Icon(
+              Icons.favorite,
+              color: Colors.white,
+            ),
+          )),
+    );
+  }
+
+  Widget buildListOfTitles() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Text("title 1"),
+        Text("title 2"),
+        Text("title 3"),
+      ],
     );
   }
 }
--- lib/resources/constants.dart
+++ lib/resources/constants.dart
@@ -1,1 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'Dimens.dart';
+
+BorderRadius borderRadius20 = BorderRadius.only(
+    topRight: Radius.circular(DIMEN_20), topLeft: Radius.circular(DIMEN_20));
+
+BoxDecoration boxDecorationSide20 = BoxDecoration(
+  borderRadius: borderRadius20,
+);
