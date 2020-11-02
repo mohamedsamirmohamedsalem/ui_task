d86036f6f8142913dac4707e612c6f427d49eccc
msalem <msalem@egypt.itgsolutions.com>
2020-11-02 11:18:40 +0200

    app run successfully on the new device

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        // visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: CourseCardDetails(),
     );
