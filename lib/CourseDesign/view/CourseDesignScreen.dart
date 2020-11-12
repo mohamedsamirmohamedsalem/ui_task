6436a0c25f1b61b3b666b28f1415707cf1492445
msalem <msalem@egypt.itgsolutions.com>
2020-11-12 14:57:30 +0200

    update to merge

--- lib/CourseDesign/view/CourseDesignScreen.dart
+++ lib/CourseDesign/view/CourseDesignScreen.dart
@@ -8,142 +8,108 @@ import 'package:ui_task/resources/Dimens.dart';
 import 'package:ui_task/resources/colors.dart';
 import 'package:ui_task/resources/constants.dart';
 
-class CourseDesignScreen extends StatefulWidget {
-  @override
-  _CourseDesignScreenState createState() => _CourseDesignScreenState();
-}
+// ignore: must_be_immutable
+class CourseDesignScreen extends StatelessWidget {
+  int categoryIndex = 0;
+  int categoryListCount = 3;
+  List<Category> categoryList = Category.categoryList;
+  List<Category> popularCourseList = Category.popularCourseList;
+  final ChangeSelectedButtonBloc _bloc = ChangeSelectedButtonBloc();
 
-class _CourseDesignScreenState extends State<CourseDesignScreen> {
-  int CategoryIndex = 0;
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: Constant.lightTheme,
-      home: Scaffold(
-        body: Container(
-            color: WHITE,
-            child: ListView(
-              children: <Widget>[
-                HomeHeaderView(context),
-                SizedBox(
-                  height: 8,
-                ),
-                SearcharBar(context),
-                SizedBox(
-                  height: 8,
-                ),
-                CategoryView(context, onCategoryListClick, CategoryIndex),
-                SizedBox(
-                  height: 8,
-                ),
-                buildDesationCollection(context),
-                SizedBox(
-                  height: 8,
-                ),
-                Container(
-                  padding: const EdgeInsets.only(
-                      top: DIMEN_8,
-                      left: DIMEN_16,
-                      right: DIMEN_16,
-                      bottom: DIMEN_24),
-                  child: Text(
-                    "Popular Course",
-                    style: TextStyle(
-                        decoration: TextDecoration.none,
-                        color: DARK_BLACK,
-                        fontWeight: FontWeight.w700,
-                        fontSize: DIMEN_21),
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                buildPopularCourse(context),
-                SizedBox(
-                  height: 8,
-                ),
-              ],
-            )),
+        debugShowCheckedModeBanner: false,
+        theme: Constant.lightTheme,
+        home: Scaffold(
+            body: Container(
+                color: WHITE,
+                child: ListView(children: <Widget>[
+                  homeHeaderView(context),
+                  SizedBox(height: DIMEN_8),
+                  searchBar(context),
+                  SizedBox(height: DIMEN_8),
+                  categoryView(context: context),
+                  SizedBox(height: DIMEN_8),
+                  buildDestinationCollection(context),
+                  SizedBox(height: DIMEN_8),
+                  buildTextWthPadding(
+                      text: popularCourse, style: kBlackTextStyle700),
+                  SizedBox(height: DIMEN_8),
+                  buildPopularCourse(context),
+                  SizedBox(height: DIMEN_8),
+                ]))));
+  }
+
+  Widget buildTextWthPadding({String text, TextStyle style}) {
+    return Container(
+        padding: EdgeInsets.only(
+            top: DIMEN_8, left: DIMEN_16, right: DIMEN_16, bottom: DIMEN_24),
+        child: Text(text, style: style));
+  }
+}
+
+  Widget homeHeaderView(context) {
+    return Container(
+        padding: const EdgeInsets.only(
+            top: DIMEN_32, left: DIMEN_16, right: DIMEN_16, bottom: DIMEN_16),
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          chooseYour,
+                          style: kBlackTextStyle400,
+                        ),
+                        buildSingleText(
+                          text: designCourse,
+                          style: kBlackTextStyle700,
+                        )
+                      ])),
+              Container(
+                width: DIMEN_35,
+                height: DIMEN_35,
+                child: Image.asset(USER_IMAGE),
+              )
+            ]));
+  }
+
+  Text buildSingleText({String text, TextStyle style}) {
+    return Text(text, style: style);
+  }
+
+  Widget searchBar(context) {
+    return Container(
+      height: DIMEN_45,
+      width: MediaQuery.of(context).size.width * 0.7,
+      alignment: Alignment.centerRight,
+      margin: const EdgeInsets.symmetric(horizontal: DIMEN_16, vertical: 10),
+      decoration: BoxDecoration(
+        color: GRAY_FILL,
+        borderRadius: BorderRadius.all(Radius.circular(DIMEN_15)),
+      ),
+      child: TextField(
+        style: TextStyle(fontSize: DIMEN_16, color: Colors.black),
+        cursorColor: GREY,
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            prefixIcon: Icon(
+              Icons.search,
+              color: GREY,
+              size: DIMEN_16,
+            ),
+            hintText: search,
+            hintStyle: TextStyle(fontSize: DIMEN_14, color: GREY)),
       ),
     );
   }
-
-  onCategoryListClick(int index) {
-    CategoryIndex = index;
-    setState(() {});
-  }
-}
-
-Widget HomeHeaderView(context) {
-  return Container(
-      padding: const EdgeInsets.only(
-          top: DIMEN_32, left: DIMEN_16, right: DIMEN_16, bottom: DIMEN_16),
-      width: MediaQuery.of(context).size.width,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width * 0.6,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  "Choose Your",
-                  style: TextStyle(
-                      decoration: TextDecoration.none,
-                      color: GRAY_ITEM_TEXT,
-                      fontWeight: FontWeight.w400,
-                      fontSize: DIMEN_18),
-                ),
-                Text(
-                  "Design Course",
-                  style: TextStyle(
-                      decoration: TextDecoration.none,
-                      color: DARK_BLACK,
-                      fontWeight: FontWeight.w700,
-                      fontSize: DIMEN_21),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: 35.0,
-            height: 35.0,
-            child: Image.asset("assets/userImage.png"),
-          ),
-        ],
-      ));
-}
-
-Widget SearcharBar(context) {
-  return Container(
-    height: 45,
-    width: MediaQuery.of(context).size.width * 0.7,
-    alignment: Alignment.centerRight,
-    margin: const EdgeInsets.symmetric(horizontal: DIMEN_16, vertical: 10),
-    decoration: BoxDecoration(
-      color: Color(0xFFE0E0E0),
-      borderRadius: BorderRadius.all(Radius.circular(15)),
-    ),
-    child: TextField(
-      style: TextStyle(fontSize: 16, color: Colors.black),
-      cursorColor: Colors.grey,
-      decoration: InputDecoration(
-          border: InputBorder.none,
-          prefixIcon: Icon(
-            Icons.search,
-            color: Colors.grey,
-            size: 16,
-          ),
-          hintText: "Search...",
-          hintStyle: TextStyle(fontSize: 14, color: Colors.grey)),
-    ),
-  );
-}
 
 Widget CategoryView(context, Function onCategoryListClick, int CategoryIndex) {
   return Column(
@@ -209,44 +175,41 @@ Widget CategoryView(context, Function onCategoryListClick, int CategoryIndex) {
   );
 }
 
-buildDesationCollection(BuildContext context) {
-  List<Category> categoryList = Category.categoryList;
-  return Container(
-    height: MediaQuery.of(context).size.height / 4.5,
-    width: MediaQuery.of(context).size.width,
-    child: ListView.builder(
-      primary: false,
-      shrinkWrap: true,
-      scrollDirection: Axis.horizontal,
-      itemCount: categoryList.length,
-      itemBuilder: (BuildContext context, int index) {
-        Category cat = categoryList[index];
-        return CategoryItem(
-          cat: cat,
-        );
-      },
-    ),
-  );
-}
-
-Widget buildPopularCourse(BuildContext context) {
-  List<Category> categoryList = Category.popularCourseList;
-  return Container(
-    //height: MediaQuery.of(context).size.height / 2,
-    child: GridView.builder(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      itemCount: categoryList.length,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 1,
+  buildDestinationCollection(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 4.5,
+      width: MediaQuery.of(context).size.width,
+      child: ListView.builder(
+        primary: false,
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: categoryList.length,
+        itemBuilder: (BuildContext context, int index) {
+          Category cat = categoryList[index];
+          return CategoryItem(
+            cat: cat,
+          );
+        },
       ),
-      itemBuilder: (context, index) {
-        return WidgetAnimator(
-            child: PopularCourse(
-          cat: categoryList[index],
-        ));
-      },
-    ),
-  );
+    );
+  }
+
+  Widget buildPopularCourse(BuildContext context) {
+    return Container(
+      child: GridView.builder(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: popularCourseList.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1,
+        ),
+        itemBuilder: (context, index) {
+          return PopularCourse(
+            cat: popularCourseList[index],
+          );
+        },
+      ),
+    );
+  }
 }
